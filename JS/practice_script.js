2ce45ae9f5f084eb91d3f730e564b4260192fd07
Collin Jones <collin.g.jones34@gmail.com>
2022-02-14 16:04:06 -0600

    More practice

---
+++ JS/practice_script.js
@@ -0,0 +1,74 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+//
+// console.log("Collin");
+// console.log(23);
+//
+// let firstName = "Collin";
+// console.log(firstName);
+//
+// //Variable name conventions
+// let collin_jones = "CJ"
+// let $function = 24;
+//
+// let person = "collin"
+// let PI = 3.1415;
+//
+// let myFirstJob = "Warehouse Worker";
+// let myCurrentJob = "Student";
+//
+// let job1 = "Warehouse Worker"
+// let job2 = "student"
+// console.log(myFirstJob)
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun)
+// console.log(typeof true);
+//
+// javascriptIsFun = "Yes!";
+// console.log(typeof javascriptIsFun)
+//
+// let year;
+// console.log(year)
+//
+// year = 1991;
+// console.log(typeof year);
+//
+// console.log(typeof null);
+
+// let age = 24;
+// age = 25;
+//
+// const birthYear = 1998;
+// // birthYear = 1990;
+// // const job;
+//
+// var job = 'Warehouse'
+// job = 'Student'
+//
+// lastName = 'Jones'
+// console.log(lastName)
+
+// const ageJones = 2037 - 1998;
+// const ageJoe = 2037 - 2018;
+// console.log(ageJones, ageJoe);
+const now = 2037;
+const ageJones = now - 1998;
+const ageJoe = now - 2018;
+console.log(ageJones, ageJoe);
+
+console.log(ageJones * 2, ageJoe / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Collin';
+const lastName = 'Jones';
+console.log(firstName, '', lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x)
--- JS/script.js
+++
@@ -1,8 +0,0 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
-
-console.log("Collin");
-console.log(23);
-
-let firstName = "Collin";
-console.log(firstName);
