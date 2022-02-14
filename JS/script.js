7891c963daa28dbcd8cfece4511823e7bd9046ae
Collin Jones <collin.g.jones34@gmail.com>
2022-02-14 14:49:42 -0600

    Adding to the Practice

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Collin");
+console.log(23);
+
+let firstName = "Collin";
+console.log(firstName);
