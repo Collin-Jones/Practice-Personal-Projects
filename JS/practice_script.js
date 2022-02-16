f0cb5f6014b77b4ab9a79d89f98ec81645bdb13c
Collin Jones <collin.g.jones34@gmail.com>
2022-02-15 23:08:05 -0600

    moved the first challenge over to the other js file

--- JS/practice_script.js
+++ JS/practice_script.js
@@ -89,39 +89,6 @@
 // const averageAge = (ageJoe + ageJones) / 2;
 // console.log(ageJones , ageJoe, averageAge)
 
-//Coding Exercise
-
-// 1. Store Mark's and John's mass and height in variables
-// 2. Calculate both their BMIs using the formula (you can even implement both
-// versions)
-// 3. Create a Boolean variable 'markHigherBMI' containing information about
-// whether Mark has a higher BMI than John.
-
-//  Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
-// m tall.
-//  Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
-// m tall.
-
-// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-// and height in meter).
-
-// const marksWeight = 78;
-// const marksHeight = 1.69;
-// const johnWeight = 92;
-// const johnHeight = 1.95;
-
-// const marksWeight = 95;
-// const marksHeight = 1.88;
-// const johnWeight = 85;
-// const johnHeight = 1.76;
-//
-// const BMIMark = marksWeight / marksHeight ** 2;
-// const BMIJohn = johnWeight / (johnHeight * johnHeight);
-//
-// const markHigherBMI = BMIMark > BMIJohn;
-//
-// console.log(BMIMark , BMIJohn);
-// console.log(markHigherBMI);
 
 // const firstName = 'Collin';
 // const job = 'student';
