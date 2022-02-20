030d7b5655e99198e435ca9839bdb4d952e01ec8
Collin Jones <collin.g.jones34@gmail.com>
2022-02-20 17:19:08 -0600

    use variables

--- practice-scriptp2.js
+++ practice-scriptp2.js
@@ -55,4 +55,16 @@ const yearsUntilRetirement = (birthYear, firstName) =>{
 }
 
 console.log(yearsUntilRetirement(1991, 'Bob'));
-console.log(yearsUntilRetirement(1980, 'Joe'))
+console.log(yearsUntilRetirement(1980, 'Joe'));
+
+const cutPieces = function (fruit){
+    return fruit * 4;
+}
+ const fruitProcessor = function (apples, oranges){
+    const applePieces = cutPieces(apples);
+     const orangePieces = cutPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
