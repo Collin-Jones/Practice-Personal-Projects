e0f0bc95d0087603a4799b29930ce82922af32de
Collin Jones <collin.g.jones34@gmail.com>
2022-02-20 17:09:54 -0600

    learned function declaration, expression, and arrow functions

--- practice-scriptp2.js
+++ practice-scriptp2.js
@@ -1,2 +1,58 @@
+// showing how to use strict mode.
 'use strict';
 
+// let hasDriversLicense = false;
+// const passTest = true;
+//
+// if(passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive!');
+
+// function logger(){
+//     console.log('My name is Collin');
+// }
+// // calling / running / invoking function
+// logger();
+//
+// function fruitProcessor(apples, oranges){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+//
+//
+// const appleJuice = fruitProcessor(5, 2);
+// console.log(appleJuice);
+//
+// const appleOrangeJuice = fruitProcessor(2 , 5);
+// console.log(appleOrangeJuice);
+
+//Function declaration
+function calcAge1(birthYear){
+    return 2037 - birthYear;
+}
+const age1 = calcAge1(1997)
+
+//Function expression
+const calAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+const age2 = calAge2(1997);
+
+console.log(age1, age2)
+
+//Arrow Functions
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1997);
+console.log(age3);
+
+
+
+
+const yearsUntilRetirement = (birthYear, firstName) =>{
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Bob'));
+console.log(yearsUntilRetirement(1980, 'Joe'))
