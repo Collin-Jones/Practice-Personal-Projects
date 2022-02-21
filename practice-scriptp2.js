3a03676694110376db0a6128cd4b3b3ff8ba9e58
Collin Jones <collin.g.jones34@gmail.com>
2022-02-20 21:07:03 -0600

    relearning arrays

--- practice-scriptp2.js
+++ practice-scriptp2.js
@@ -26,45 +26,66 @@
 // console.log(appleOrangeJuice);
 
 //Function declaration
-function calcAge1(birthYear){
-    return 2037 - birthYear;
-}
-const age1 = calcAge1(1997)
-
-//Function expression
-const calAge2 = function (birthYear){
-    return 2037 - birthYear;
-}
-const age2 = calAge2(1997);
-
-console.log(age1, age2)
-
-//Arrow Functions
-const calcAge3 = birthYear => 2037 - birthYear;
-const age3 = calcAge3(1997);
-console.log(age3);
-
-
+// function calcAge1(birthYear){
+//     return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1997)
+//
+// //Function expression
+// const calAge2 = function (birthYear){
+//     return 2037 - birthYear;
+// }
+// const age2 = calAge2(1997);
+//
+// console.log(age1, age2)
+//
+// //Arrow Functions
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1997);
+// console.log(age3);
 
 
-const yearsUntilRetirement = (birthYear, firstName) =>{
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // return retirement
-    return `${firstName} retires in ${retirement} years.`;
-}
 
-console.log(yearsUntilRetirement(1991, 'Bob'));
-console.log(yearsUntilRetirement(1980, 'Joe'));
 
-const cutPieces = function (fruit){
-    return fruit * 4;
-}
- const fruitProcessor = function (apples, oranges){
-    const applePieces = cutPieces(apples);
-     const orangePieces = cutPieces(oranges);
-    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
-    return juice;
-}
+// const yearsUntilRetirement = (birthYear, firstName) =>{
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement
+//     return `${firstName} retires in ${retirement} years.`;
+// }
+//
+// console.log(yearsUntilRetirement(1991, 'Bob'));
+// console.log(yearsUntilRetirement(1980, 'Joe'));
+//
+// const cutPieces = function (fruit){
+//     return fruit * 4;
+// }
+//  const fruitProcessor = function (apples, oranges){
+//     const applePieces = cutPieces(apples);
+//      const orangePieces = cutPieces(oranges);
+//     const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+//     return juice;
+// }
+//
+// console.log(fruitProcessor(2, 3));
 
-console.log(fruitProcessor(2, 3));
+//Arrays
+const friend1 = 'Micheal';
+const friend2 = 'Steve';
+const friend3 = 'Peter';
+
+
+const friends = ['Micheal', 'Steven', 'Peter', 'Collin'];
+console.log(friends);
+
+const years = new Array(1991, 1999, 1990, 2020);
+
+console.log(friends[0]);
+console.log((friends [2]));
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
