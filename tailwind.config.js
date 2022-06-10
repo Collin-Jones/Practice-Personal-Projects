a08173321e735b5f4a861da129af5a369953b814
Collin Jones <collin.g.jones34@gmail.com>
2022-06-10 13:13:11 -0500

    Adding tailwind to my personal projects

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./HTML/Practice.html"],
+    theme: {
+        extend: {
+            width: {
+                'input-width-sm' : '200px',
+                'input-width-lg' : '300px'
+            },
+            colors: {
+                'primary' : '#002254',
+                'callToAction' : '#FFCA36'
+            },
+        },
+    },
+    plugins: [],
+}
