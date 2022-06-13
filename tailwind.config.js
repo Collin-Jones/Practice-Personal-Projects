3f7034b7e4bbb320eadd58f7732ec5eef6a3d559
colli <collin.g.jones34@gmail.com>
2022-06-13 00:07:57 -0500

    doing a coding challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   important: true,
-  content: ["./main/HTML/Practice.html"],
+  content: ["./main/HTML/Practice.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-// npx tailwindcss -i /CSS/input.css -o /CSS/output.css --watch
+
