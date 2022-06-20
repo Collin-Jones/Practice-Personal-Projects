19dfeee2e63bda41922e2aa7461c0f3ee71a0617
colli <collin.g.jones34@gmail.com>
2022-06-20 12:15:27 -0500

    testing

---
+++ main-course/Projects/project2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.open-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < ; i++) {
+    
+}
