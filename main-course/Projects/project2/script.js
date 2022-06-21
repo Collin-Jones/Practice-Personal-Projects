3e49becf15aa574f5e1458c52b8fb062f783983b
colli <collin.g.jones34@gmail.com>
2022-06-21 12:37:36 -0500

    second project is done

--- main-course/Projects/project2/script.js
+++ main-course/Projects/project2/script.js
@@ -3,10 +3,27 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.open-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openmodal = function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+};
 
-for (let i = 0; i < ; i++) {
-    
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openmodal)
+    btnCloseModal.addEventListener('click', closeModal)
+    overlay.addEventListener('click', closeModal);
 }
+
+document.addEventListener('keydown', function (e){
+    console.log(e.key)
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+       closeModal();
+    }
+})
